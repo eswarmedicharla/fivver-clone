8ca82fd61ed0f47b094b0adcc50c76e4e3894933
eswarmedicharla <eswarmedicharla66@gmail.com>
2023-10-04 18:07:54 +0530

    Fix line endings according to .gitattributes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 function App() {
+  const Layout = () => {
+    return (
+      <div className='App'>
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+
+    )
+  }
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />
+    }
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RouterProvider router={router} />
     </div>
   );
 }
